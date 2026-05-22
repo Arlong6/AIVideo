acdd5b97194da0edbc32b3d30990ea7705d0ccd9
Tony <tony0912045596@gmail.com>
2026-05-23 00:16:15 +0800

    test(pixel-battle): update screw_dart/shard_scatter range assertions for numeric range

--- pixel_battle/tests/test_skill_cooldown.py
+++ pixel_battle/tests/test_skill_cooldown.py
@@ -12,7 +12,7 @@ def test_brick_has_screw_dart_cd_skill():
     assert cd_skills[0].id == "screw_dart"
     assert cd_skills[0].cooldown_ms == 4000
     assert cd_skills[0].dmg == 5
-    assert cd_skills[0].range == "special"
+    assert cd_skills[0].range == 250
 
 
 def test_glass_has_shard_scatter_cd_skill():
@@ -22,7 +22,7 @@ def test_glass_has_shard_scatter_cd_skill():
     assert cd_skills[0].id == "shard_scatter"
     assert cd_skills[0].cooldown_ms == 4000
     assert cd_skills[0].dmg == 4
-    assert cd_skills[0].range == "special"
+    assert cd_skills[0].range == 240
     assert cd_skills[0].stagger_ms == 500
 
 
